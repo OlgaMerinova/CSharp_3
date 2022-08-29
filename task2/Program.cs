59bc15cf2ee637ff3781dce9cf09b97e5be9aba6
olja4567 <olja4567@yandex.ru>
2022-08-29 16:27:56 +0300

    добавлено решение 3х задач

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+Console.Write("введите пятизначное число:   ");
+bool palindrome = true;
+int x = int.Parse(Console.ReadLine());
+int c = x.ToString().Length - 1;
+string b = x.ToString();
+for (int i = 0; i < c; i++)
+{
+    if (b[i] != b[c - i])
+        palindrome = false;
+        if (palindrome == true)
+            Console.Write("Число является палиндромом");
+        else Console.Write("Число не является палиндромом");
+    Console.ReadKey();
+}    
+
+
---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. 
+//Полученный массив вывести на экран.
+//3 -> [1, 8, 27]
+//5 -> [1, 8, 27, 64, 125]
+
+
+Console.Write("Пожалуйста, введите число:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [N];
+
+int index = 0;
+int num = 1;
+while (index < N)
+{
+    array[index] = num*num*num;
+    Console.Write(array[index] + " ");
+    num++;
+    index++;
+}
+
+
